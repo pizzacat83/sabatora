e85af7c28052364629688e1314b2918baee55770
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2024-12-30 18:17:33 +0900

    typo

--- saba_core/src/http.rs
+++ saba_core/src/http.rs
@@ -29,7 +29,7 @@ impl Header {
 
 impl HttpResponse {
     pub fn new(raw_response: String) -> Result<Self, Error> {
-        let preprocessed_response = raw_response.trim_start().replace("\n\r", "\n");
+        let preprocessed_response = raw_response.trim_start().replace("\r\n", "\n");
 
         let (status_line, remaining) =
             preprocessed_response
