b63ee2b230ac9c54ad604a7f17cb4aba6c9a1e67
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2024-12-30 09:42:03 +0900

    add local server

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use noli::prelude::*;
 
 fn main() -> u64 {
     let client = HttpClient::new();
-    match client.get("example.com".to_string(), 80, "/".to_string()) {
+    match client.get("host.test".to_string(), 8000, "/simple.html".to_string()) {
         Ok(res) => {
             print!("response:\n{:#?}", res);
         }
