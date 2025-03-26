b96b0691c0390f595ce12a62d83f01a7869489a2
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2025-03-26 22:13:33 +0900

    painter

--- saba_core/src/renderer/layout/paint.rs
+++ saba_core/src/renderer/layout/paint.rs
@@ -1,60 +1,93 @@
-use crate::display_item::DisplayItem;
+use crate::display_item::{self, DisplayItem};
 use crate::renderer::layout::computed_style::{ComputedStyle, DisplayType};
 use crate::renderer::layout::layout_object::LayoutPoint;
 use alloc::vec;
 use alloc::vec::Vec;
 
-use super::position::PositionedBlockBox;
+use super::position::{
+    PositionedBlockBox, PositionedBlockBoxChildren, PositionedInlineBox, PositionedLineBox,
+};
 
 pub fn paint(tree: PositionedBlockBox) -> Vec<DisplayItem> {
-    // TODO
-    vec![
-        DisplayItem::Text {
-            text: "inline1 inline1 inline1".into(),
-            style: ComputedStyle {
-                display: Some(DisplayType::Inline),
-            },
-            layout_point: LayoutPoint { x: 0, y: 0 },
-        },
-        DisplayItem::Text {
-            text: "inline2 inline2 inline2".into(),
-            style: ComputedStyle {
-                display: Some(DisplayType::Inline),
-            },
-            layout_point: LayoutPoint { x: 8 * 23, y: 0 },
-        },
-        DisplayItem::Text {
-            text: "inline3 inline3 inline3".into(),
-            style: ComputedStyle {
-                display: Some(DisplayType::Inline),
-            },
-            layout_point: LayoutPoint {
-                x: 8 * 23 * 2,
-                y: 0,
-            },
-        },
-        DisplayItem::Text {
-            text: "block4 block4 block4".into(),
-            style: ComputedStyle {
-                display: Some(DisplayType::Inline),
-            },
-            layout_point: LayoutPoint { x: 0, y: 16 },
-        },
-        DisplayItem::Text {
-            text: "block5 block5 block5".into(),
-            style: ComputedStyle {
-                display: Some(DisplayType::Inline),
-            },
-            layout_point: LayoutPoint { x: 0, y: 16 * 2 },
-        },
-        DisplayItem::Text {
-            text: "inline6 inline6 inline6".into(),
-            style: ComputedStyle {
-                display: Some(DisplayType::Inline),
-            },
-            layout_point: LayoutPoint { x: 0, y: 16 * 3 },
-        },
-    ]
+    paint_block(tree, &LayoutPoint { x: 0, y: 0 })
+}
+
+fn paint_block(tree: PositionedBlockBox, left_top: &LayoutPoint) -> Vec<DisplayItem> {
+    match tree.children {
+        PositionedBlockBoxChildren::Empty => Vec::new(),
+        PositionedBlockBoxChildren::Blocks(blocks) => {
+            let mut left_top = left_top.clone();
+            let mut display_items = Vec::new();
+
+            for block in blocks {
+                let block_height = block.size.height;
+                display_items.extend(paint_block(block, &left_top));
+                // Blocks stack vertically.
+                left_top = LayoutPoint {
+                    x: left_top.x,
+                    y: left_top.y + block_height,
+                }
+            }
+            display_items
+        }
+        PositionedBlockBoxChildren::Inlines(lines) => {
+            let mut left_top = left_top.clone();
+            let mut display_items = Vec::new();
+
+            for line in lines {
+                let line_height = line.size.height;
+                display_items.extend(paint_line(line, &left_top));
+                // Lines stack vertically.
+                left_top = LayoutPoint {
+                    x: left_top.x,
+                    y: left_top.y + line_height,
+                }
+            }
+            display_items
+        }
+    }
+}
+
+fn paint_line(line: PositionedLineBox, left_top: &LayoutPoint) -> Vec<DisplayItem> {
+    let mut left_top = left_top.clone();
+    let mut display_item = Vec::new();
+
+    for inline in line.children {
+        let inline_width = inline.size.width;
+        display_item.extend(paint_inline(inline, &left_top));
+        // Inline items stack horizontally.
+        left_top = LayoutPoint {
+            x: left_top.x + inline_width,
+            y: left_top.y,
+        }
+    }
+
+    display_item
+}
+
+fn paint_inline(inline: PositionedInlineBox, left_top: &LayoutPoint) -> Vec<DisplayItem> {
+    let mut display_item = Vec::new();
+    if let Some(text) = inline.text {
+        display_item.push(DisplayItem::Text {
+            text,
+            style: inline.style,
+            layout_point: left_top.clone(),
+        });
+        assert!(inline.children.is_empty());
+    }
+
+    let mut left_top = left_top.clone();
+    for inline in inline.children {
+        let inline_width = inline.size.width;
+        display_item.extend(paint_inline(inline, &left_top));
+        // Inline items stack horizontally.
+        left_top = LayoutPoint {
+            x: left_top.x + inline_width,
+            y: left_top.y,
+        };
+    }
+
+    display_item
 }
 
 #[cfg(test)]
