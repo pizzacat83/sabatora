a736ae5c7a1b3a2e52732e684604c75317c0cbdb
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2025-08-30 14:37:46 +0900

    eval 1+2

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -1,8 +1,10 @@
+use core::ops::Add;
+
 use alloc::format;
 use alloc::rc::Rc;
 use alloc::string::String;
 
-use crate::renderer::js::ast::Node;
+use crate::renderer::js::ast::{AdditiveOperator, Node};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum RuntimeValue {
@@ -19,9 +21,40 @@ impl JsRuntime {
         JsRuntime {}
     }
 
+    // TODO: How should we model "exceptions"?
     // TODO: Why &Rc instead of Rc?
     fn eval(&mut self, node: &Rc<Node>) -> Result<RuntimeValue> {
         match node.as_ref() {
+            Node::ExpressionStatement(expr) => self.eval(expr),
+            Node::AdditiveExpression {
+                operator,
+                left,
+                right,
+            } => {
+                let left_value = self.eval(left)?;
+                let right_value = self.eval(right)?;
+
+                match operator {
+                    AdditiveOperator::Addition => {
+                        if let (RuntimeValue::Number(left), RuntimeValue::Number(right)) =
+                            (left_value, right_value)
+                        {
+                            Ok(RuntimeValue::Number(left + right))
+                        } else {
+                            Err(format!("Unsupported addition: ({left:?}) + ({right:?})"))
+                        }
+                    }
+                    AdditiveOperator::Subtract => {
+                        if let (RuntimeValue::Number(left), RuntimeValue::Number(right)) =
+                            (left_value, right_value)
+                        {
+                            Ok(RuntimeValue::Number(left + right))
+                        } else {
+                            Err(format!("Unsupported subtraction: ({left:?}) - ({right:?})"))
+                        }
+                    }
+                }
+            }
             Node::NumericLiteral(num) => Ok(RuntimeValue::Number(*num)),
             node => Err(format!("Unsupported node: {node:?}")),
         }
@@ -37,6 +70,20 @@ mod tests {
     #[test]
     fn test_num() {
         let input = "42;";
+        let lexer = JsLexer::new(input.into());
+        let mut parser = JsParser::new(lexer);
+        let program = parser.parse();
+
+        let mut runtime = JsRuntime::new();
+        for node in program.body {
+            let result = runtime.eval(&node);
+            dbg!(&result);
+        }
+    }
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2;";
         let lexer = JsLexer::new(input.into());
         let mut parser = JsParser::new(lexer);
         let program = parser.parse();
