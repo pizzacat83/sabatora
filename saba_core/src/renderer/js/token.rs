31a22a7fc157967b81df77f140138995f2c6b9c9
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2025-08-30 13:05:16 +0900

    tokenize 1 + 2

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -21,13 +21,71 @@ impl JsLexer {
             input: js.chars().collect(),
         }
     }
+
+    fn consume_number(&mut self) -> Token {
+        let mut num = 0;
+        loop {
+            if self.pos >= self.input.len() {
+                return Token::Number(num);
+            }
+
+            let c = self.input[self.pos];
+            match c {
+                '0'..='9' => {
+                    num = num * 10 + (c.to_digit(10).unwrap() as u64);
+                    self.pos += 1;
+                }
+                _ => break,
+            }
+        }
+        return Token::Number(num);
+    }
 }
 
 impl Iterator for JsLexer {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // TODO
-        None
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        // skip whitespace
+        // TODO: But in JS, a newline can terminate a sentence, right?
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | ';' | '=' | '(' | ')' | '{' | '}' | ',' | '.' => {
+                let t = Token::Punctuator(c);
+                self.pos += 1;
+                t
+            }
+            '0'..='9' => self.consume_number(),
+            _ => unimplemented!("char '{c}' is not supported yet"),
+        };
+
+        Some(token)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let mut lexer = JsLexer::new(input);
+        let tokens: Vec<_> = lexer.collect();
+
+        dbg!(&tokens);
     }
 }
